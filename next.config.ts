9eda1c61d1a6267cba17f134da2364ffb5ea00f2
Yash Company <yn10122007@gmail.com>
2025-06-23 03:44:37 +0000

    I have pushed this code to github but to run it on github pages I need t

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
+const repo = 'YashVerse';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Configure basePath and assetPrefix for GitHub Pages
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +16,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    // Un-optimize images since they are not supported on static export
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
