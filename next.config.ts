a4816c73167a4ad281674dc9631cb7729d8caaa1
Yash Company <yn10122007@gmail.com>
2025-06-24 03:56:09 +0000

    I previously pushed this file to repository which I later renamed. HOw d

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type {NextConfig} from 'next';
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
-const repo = 'YashVerse';
+const repo = process.env.GITHUB_REPOSITORY
+  ? process.env.GITHUB_REPOSITORY.split('/')[1]
+  : '';
 
 const nextConfig: NextConfig = {
   ...(isGithubActions && {output: 'export'}),
-  // Configure basePath and assetPrefix for GitHub Pages
   basePath: isGithubActions ? `/${repo}` : '',
   assetPrefix: isGithubActions ? `/${repo}/` : '',
   typescript: {
@@ -16,7 +17,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    // Un-optimize images since they are not supported on static export
     unoptimized: true,
     remotePatterns: [
       {
