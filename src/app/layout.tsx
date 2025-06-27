0ab62c3b898b41ca62dded17e865ca0d2049c95f
Yash Company <yn10122007@gmail.com>
2025-06-27 00:40:16 +0000

    instead of a .ico can u use .jpg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'YashVerse',
   description: 'The personal portfolio of Yash.',
+  icons: {
+    icon: '/favicon.jpg',
+  },
 };
 
 export default function RootLayout({
